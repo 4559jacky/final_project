b074a2f4b4d8ccbdc2b20d297cfcf281b10f07e3
4559jacky <4559jacky@gmail.com>
2025-04-06 03:20:45 +0900

    [jji] approval admin/user service 클래스 생성 commit

--- groupware/src/main/java/com/mjc/groupware/approval/admin/service/AdminApprovalService.java
+++ groupware/src/main/java/com/mjc/groupware/approval/admin/service/AdminApprovalService.java
@@ -1,5 +1,8 @@
 package com.mjc.groupware.approval.admin.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminApprovalService {
 
 }
--- groupware/src/main/java/com/mjc/groupware/approval/user/service/UserApprovalService.java
+++ groupware/src/main/java/com/mjc/groupware/approval/user/service/UserApprovalService.java
@@ -1,5 +1,8 @@
 package com.mjc.groupware.approval.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserApprovalService {
 
 }
