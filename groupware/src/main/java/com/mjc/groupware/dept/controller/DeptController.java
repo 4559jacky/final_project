28a7c33bf81e7eeb78038e1fd2272fe67ceac2df
Jayden <jayden6849@gmail.com>
2025-04-08 08:31:14 +0900

    [cjs] 풀리퀘스트 준비 - 동작 확인 완료

---
+++ groupware/src/main/java/com/mjc/groupware/dept/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.mjc.groupware.dept.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DeptController {
+	
+	@GetMapping("/dept/create")
+	public String createDeptView() {
+		return "dept/create";
+	}
+	
+}
