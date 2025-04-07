7029c224b9091a300bbf93305edd836f88465fd2
kdy0241 <dgwag489@gmail.com>
2025-04-07 15:36:14 +0900

    [kdy] 게시판 시작 해야함 commit

---
+++ groupware/src/main/java/com/mjc/groupware/board/controller/BoardController.java
@@ -0,0 +1,8 @@
+package com.mjc.groupware.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+
+}
---
+++ groupware/src/main/java/com/mjc/groupware/board/service/BoardService.java
@@ -0,0 +1,8 @@
+package com.mjc.groupware.board.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+}
