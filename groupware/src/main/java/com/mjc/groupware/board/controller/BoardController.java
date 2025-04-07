0c2c8601283f37e6e6266135a76154124e7a3979
kdy0241 <dgwag489@gmail.com>
2025-04-07 20:40:58 +0900

    [kdy] 게시글 등록 만드는중 commit

--- groupware/src/main/java/com/mjc/groupware/board/controller/BoardController.java
+++ groupware/src/main/java/com/mjc/groupware/board/controller/BoardController.java
@@ -1,8 +1,27 @@
 package com.mjc.groupware.board.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
-
+	@GetMapping("/board/create")
+	public String createBoardView() {
+		return "board/create";
+	}
+//	@PostMapping("/board")
+//	@ResponseBody
+//	public Map<String,String> createBoardApi(BoardDto dto){
+//		Map<String,String> resultMap = new HashMap<String,String>();
+//		resultMap.put("res_code", "404");
+//		resultMap.put("res_msg", "게시글 등록중 오류가 발생했습니다.");
+//		
+//		return resultMap;
+//	}
 }
---
+++ groupware/src/main/java/com/mjc/groupware/board/dto/BoardDto.java
@@ -0,0 +1,20 @@
+//package com.mjc.groupware.board.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@Builder
+//public class BoardDto {
+//	private Long board_no;
+//	private String board_title;
+//	private String board_content;
+//}
