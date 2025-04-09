d70e80780c07260a1fb4d3bce5d6e251f4a1f9d3
Jayden <jayden6849@gmail.com>
2025-04-09 16:55:54 +0900

    [cjs] 다시 되돌린 상태. - 직급 조직도 예정

--- groupware/src/main/java/com/mjc/groupware/member/repository/MemberRepository.java
+++ groupware/src/main/java/com/mjc/groupware/member/repository/MemberRepository.java
@@ -1,11 +1,16 @@
 package com.mjc.groupware.member.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.mjc.groupware.dept.entity.Dept;
 import com.mjc.groupware.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	
 	Member findByMemberId(String keyword);
+	
+	List<Member> findByDept(Dept dept);
 	
 }
