da8dd75cd7860d84cc3678d61f10d89689598148
sbsophie <devsophie37@gmail.com>
2025-04-09 15:59:18 +0900

    [psb]일정추가,상세 modal분리 commit

--- groupware/src/main/java/com/mjc/groupware/plan/controller/PlanController.java
+++ groupware/src/main/java/com/mjc/groupware/plan/controller/PlanController.java
@@ -1,14 +1,45 @@
 package com.mjc.groupware.plan.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mjc.groupware.plan.dto.PlanDto;
+import com.mjc.groupware.plan.entity.Plan;
+import com.mjc.groupware.plan.service.PlanService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class PlanController {
 	
+	private final PlanService planService;
+
 	@GetMapping("/calendar")
 	public String calendarView() {
 		return "plan/calendar";
 	}
+
+	@PostMapping("/plan/create")
+	@ResponseBody
+	public Map<String,String> createPlanApi(PlanDto dto){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "일정 등록중 오류가 발생했습니다.");
+		
+		Plan result = planService.savePlan(dto);
+		if(result != null) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "일정이 등록되었습니다.");
+		}
+		return resultMap;
+	}
+
+	
 	
 }
--- groupware/src/main/java/com/mjc/groupware/plan/dto/PlanDto.java
+++ groupware/src/main/java/com/mjc/groupware/plan/dto/PlanDto.java
@@ -1,6 +1,6 @@
 package com.mjc.groupware.plan.dto;
 
-import com.mjc.groupware.plan.dto.PlanDto;
+import com.mjc.groupware.plan.entity.Plan;
 
 import groovy.transform.ToString;
 import lombok.AllArgsConstructor;
@@ -17,7 +17,48 @@ import lombok.Setter;
 @Builder
 public class PlanDto {
 
-	private int plan_no;
+	private Long plan_no;
+	private String plan_name;
+    private String plan_content;
+    private String start_date;
+    private String end_date;
+    private String color;
+    private int reg_member_no;
+    private String repeat_yn;
+    private String delete_yn;
+    private String allday_yn;
+    private String plan_type;
 	
+	public Plan toEntity() {
+		return Plan.builder()
+				.planNo(plan_no)
+				.planName(plan_name)
+				.planContent(plan_content)
+				.startDate(start_date)
+				.endDate(end_date)
+				.color(color)
+				.regMemberNo(reg_member_no)
+				.repeatYn(repeat_yn)
+				.deleteYn(delete_yn)
+				.alldayYn(allday_yn)
+				.planType(plan_type)
+				.build();
+	}
+	
+	public PlanDto toDto(Plan plan) {
+		return PlanDto.builder()
+				.plan_no(plan.getPlanNo())
+				.plan_name(plan.getPlanName())
+				.plan_content(plan.getPlanContent())
+				.start_date(plan.getStartDate())
+				.end_date(plan.getEndDate())
+				.color(plan.getColor())
+				.reg_member_no(plan.getRegMemberNo())
+				.repeat_yn(plan.getRepeatYn())
+				.delete_yn(plan.getDeleteYn())
+				.allday_yn(plan.getAlldayYn())
+				.plan_type(plan.getPlanType())
+				.build();
+	}
 	
 }
