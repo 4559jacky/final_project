8b39b1d1a0dd2a684cb01ba8a039b6e7cbfded97
4559jacky <4559jacky@gmail.com>
2025-04-10 15:05:36 +0900

    [jji] commit test

--- groupware/src/main/java/com/mjc/groupware/approval/entity/ApprovalForm.java
+++ groupware/src/main/java/com/mjc/groupware/approval/entity/ApprovalForm.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor; 
 
 @Entity
 @Table(name="approval_form")
