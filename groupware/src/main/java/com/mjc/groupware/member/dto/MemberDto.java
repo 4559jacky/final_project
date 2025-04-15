74037d3e757f4c18fb4b7663ecf071aa880f7a66
Jayden <jayden6849@gmail.com>
2025-04-15 17:29:16 +0900

    [cjs] 동작 확인 완료 -> PR

--- groupware/src/main/java/com/mjc/groupware/member/dto/MemberDto.java
+++ groupware/src/main/java/com/mjc/groupware/member/dto/MemberDto.java
@@ -37,7 +37,7 @@ public class MemberDto {
 	private String member_phone;
 	private Long pos_no;
 	private Long dept_no;
-	private Long role_no = (long) 3;
+	private Long role_no = (long) 2;
 	private int status = 100;
 	private String dept_name; // 04/09 JJI 사원 부서명 불러오기위해 추가
 	private String pos_name; // 04/09 JJI 사원 직급명 불러오기위해 추가
