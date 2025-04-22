9d0ef17878f19ae5640f17c9d5689ddd43f3f27f
3412ysw <3412ysw@gmail.com>
2025-04-22 17:34:47 +0900

    [ysw] 채팅방 추가 조건 변경

--- groupware/src/main/java/com/mjc/groupware/chat/controller/ChatController.java
+++ groupware/src/main/java/com/mjc/groupware/chat/controller/ChatController.java
@@ -1,14 +1,18 @@
 package com.mjc.groupware.chat.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mjc.groupware.chat.dto.ChatRoomDto;
 import com.mjc.groupware.chat.entity.ChatRoom;
 import com.mjc.groupware.chat.service.ChatRoomService;
-import com.mjc.groupware.member.entity.Member;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +23,7 @@ public class ChatController {
 
 	private final ChatRoomService chatRoomService;
 	
+	// 채팅방 페이지 전환
 	@GetMapping("/chat")
 	public String meetingRoomView(HttpSession session,Model model) {
 		
@@ -26,6 +31,24 @@ public class ChatController {
 		model.addAttribute("chatRoomList",resultList);
 		
 		return "/chat/chat";
+	}
+	
+	// 채팅방 생성
+	@PostMapping("/create")
+	@ResponseBody
+	public Map<String,String> createChatRoom(ChatRoomDto dto) {
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "채팅방 추가 중 오류가 발생하였습니다.");
+		
+		int result = chatRoomService.createChatRoom(dto);
+		
+		if(result>0) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "채팅방 추가가 완료되었습니다.");
+		}
+		
+		return resultMap;
 	}
 	
 }
