9402c71b2a56c87fc507a704541a9b989e1f5702
4559jacky <4559jacky@gmail.com>
2025-04-23 15:13:37 +0900

    [jji] 보낸 문서함(보낸 결재) 검색, 필터, 페이징 처리 구현 commit

--- groupware/src/main/java/com/mjc/groupware/approval/repository/ApprovalRepository.java
+++ groupware/src/main/java/com/mjc/groupware/approval/repository/ApprovalRepository.java
@@ -2,12 +2,16 @@ package com.mjc.groupware.approval.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mjc.groupware.approval.entity.Approval;
+import com.mjc.groupware.board.entity.Board;
 
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
-	
+	Page<Approval> findAll(Specification<Approval> spec, Pageable pageable);
 	
 	// Approval 엔티티에서 member.memberNo가 일치하는 모든 데이터 찾기
 	
