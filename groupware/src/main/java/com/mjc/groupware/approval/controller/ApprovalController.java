f5b205c828bcb898a4af2e70ecd3df03fd31d1f5
4559jacky <4559jacky@gmail.com>
2025-04-25 12:30:06 +0900

    [jji] 보낸 문서함, 받은 문서함 결재 상세페이지 결재자, 합의자 서명테이블

--- groupware/src/main/java/com/mjc/groupware/approval/controller/ApprovalController.java
+++ groupware/src/main/java/com/mjc/groupware/approval/controller/ApprovalController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mjc.groupware.approval.dto.ApprovalDto;
 import com.mjc.groupware.approval.dto.ApprovalFormDto;
 import com.mjc.groupware.approval.dto.ApprovalRelatedDto;
