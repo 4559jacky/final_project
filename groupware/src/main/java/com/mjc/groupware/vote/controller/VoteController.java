04eb8d2449e685c0273b16e55b86f358be67fbcd
kdy0241 <dgwag489@gmail.com>
2025-04-30 13:48:54 +0900

    [kdy] 댓글/대댓글 html,css 부분 수정 완료

---
+++ groupware/src/main/java/com/mjc/groupware/vote/controller/VoteController.java
@@ -0,0 +1,24 @@
+package com.mjc.groupware.vote.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class VoteController {
+	
+//	@GetMapping("/vote/create")
+//	public String createVoteView(Model model) {
+//		model.addAttribute("voteDto", new VoteDto());
+//		return "vote/create";
+//	}
+//	
+//	// 투표 작성
+//	@PostMapping("/vote")
+//	public Map<String, String> createVote(VoteDto dto){
+//		Map<String,String> resultMap = new HashMap<>();
+//		resultMap.put("res_code", "500");
+//		resultMap.put("res_msg", "투표 등록 중 오류가 발생하였습니다.");
+//		return resultMap;
+//	}
+	
+}
---
+++ groupware/src/main/java/com/mjc/groupware/vote/specification/VoteSpecification.java
@@ -0,0 +1,5 @@
+package com.mjc.groupware.vote.specification;
+
+public class VoteSpecification {
+
+}
