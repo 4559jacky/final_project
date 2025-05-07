5d170b3e9a9dc772b192c1d8e1b22872acbe6807
3412ysw <3412ysw@gmail.com>
2025-05-07 12:19:05 +0900

    [ysw] 채팅방 안 읽음 뱃지 오류 해결

---
+++ groupware/src/main/java/com/mjc/groupware/chat/session/SessionRegistry.java
@@ -0,0 +1,30 @@
+package com.mjc.groupware.chat.session;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionRegistry {
+	// 해당 멤버가 어떤 세션 ID로 접속했는지 확인하는 클래스
+	
+	// 멤버 번호 - 세션 ID	저장
+    private final Map<Long, String> memberToSession = new ConcurrentHashMap<>();
+
+    // 특정 유저가 웹소캣 연결되면 세션 ID 저장
+    public void register(Long memberNo, String sessionId) {
+        memberToSession.put(memberNo, sessionId);
+    }
+
+    //유저 번호로 어떤 세션 ID를 가졌는지 확인
+    public String getSessionIdForMember(Long memberNo) {
+        return memberToSession.get(memberNo);
+    }
+
+    // 세션 끊겼을 때 모든 유저의 세션 중 이 세션 ID를 가진 유저를 제거
+    public void unregister(String sessionId) {
+        memberToSession.values().removeIf(v -> v.equals(sessionId));
+    }
+}
+
