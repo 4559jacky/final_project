93bdbb3ca0b215022149b93c90f9aff18106d965
kdy0241 <dgwag489@gmail.com>
2025-05-13 15:50:58 +0900

    [kdy] 투표 작성할때 투표 수정하는곳에서 항목이 뺀 상태로 다시 수정하고,삭제하면 추가해서 나오는거 다시 수정함 commit

--- groupware/src/main/java/com/mjc/groupware/vote/repository/VoteOptionRepository.java
+++ groupware/src/main/java/com/mjc/groupware/vote/repository/VoteOptionRepository.java
@@ -1,12 +1,18 @@
 package com.mjc.groupware.vote.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.mjc.groupware.vote.entity.Vote;
 import com.mjc.groupware.vote.entity.VoteOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface VoteOptionRepository extends JpaRepository<VoteOption, Long> {
     List<VoteOption> findByVote(Vote vote);
 
     List<VoteOption> findByVote_VoteNo(Long voteNo); // ✅ 연관관계 경로에 맞게 수정
+    
+    @Transactional
+    void deleteAllByVote_VoteNo(Long voteNo);
 }
