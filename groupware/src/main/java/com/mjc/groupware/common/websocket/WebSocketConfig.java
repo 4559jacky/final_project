fab731c01e3043aa85153f20d063e950ee429924
3412ysw <3412ysw@gmail.com>
2025-05-19 11:18:37 +0900

    [ysw] local 웬소캣수정 반영

--- groupware/src/main/java/com/mjc/groupware/common/websocket/WebSocketConfig.java
+++ groupware/src/main/java/com/mjc/groupware/common/websocket/WebSocketConfig.java
@@ -11,7 +11,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws-chat")
-				.setAllowedOrigins("*")
+				.setAllowedOrigins("http://localhost:8080")
 				.withSockJS();
 	}
 	@Override
